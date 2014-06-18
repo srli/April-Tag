3e7ec8bcb369c30857e09dd419761f6ded9ffd17
Sophia Li <sophia.li@students.olin.edu>
2014-06-18 16:47:55 -0400

    readme

---
+++ pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sophie/apriltags/src/Edge.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Edge.cc.o"
+  "/home/sophie/apriltags/src/FloatImage.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/FloatImage.cc.o"
+  "/home/sophie/apriltags/src/GLine2D.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLine2D.cc.o"
+  "/home/sophie/apriltags/src/GLineSegment2D.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLineSegment2D.cc.o"
+  "/home/sophie/apriltags/src/Gaussian.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Gaussian.cc.o"
+  "/home/sophie/apriltags/src/GrayModel.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GrayModel.cc.o"
+  "/home/sophie/apriltags/src/Homography33.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Homography33.cc.o"
+  "/home/sophie/apriltags/src/MathUtil.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/MathUtil.cc.o"
+  "/home/sophie/apriltags/src/Quad.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Quad.cc.o"
+  "/home/sophie/apriltags/src/Segment.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Segment.cc.o"
+  "/home/sophie/apriltags/src/TagDetection.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetection.cc.o"
+  "/home/sophie/apriltags/src/TagDetector.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetector.cc.o"
+  "/home/sophie/apriltags/src/TagFamily.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagFamily.cc.o"
+  "/home/sophie/apriltags/src/UnionFindSimple.cc" "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/src/UnionFindSimple.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "../build/include"
+  "../AprilTags"
+  "../."
+  "/opt/local/include"
+  "/opt/ros/hydro/include/opencv"
+  "/opt/ros/hydro/include"
+  "/usr/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ pod-build/example/CMakeFiles/imu.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sophie/apriltags/example/Serial.cpp" "/home/sophie/apriltags/pod-build/example/CMakeFiles/imu.dir/Serial.cpp.o"
+  "/home/sophie/apriltags/example/imu.cpp" "/home/sophie/apriltags/pod-build/example/CMakeFiles/imu.dir/imu.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sophie/apriltags/pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "../build/include"
+  "../AprilTags"
+  "../."
+  "/opt/local/include"
+  "/opt/ros/hydro/include/opencv"
+  "/opt/ros/hydro/include"
+  "/usr/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
